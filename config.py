e2d7a4f4997f0613c65bf6c72964a8021c2bba95
Haruki <127585577+Aaaakkkkbot@users.noreply.github.com>
2024-12-02 15:37:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,25 +3,25 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "21418386"))
+API_HASH = os.environ.get("API_HASH", "aeac46b1d123e82fe6dcb43b6a26cfae")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6149872961:AAEJnhMAUSRymR5AlED38f8n6fNQSJ38Ego")
+ADMIN = int(os.environ.get("ADMIN", "5496176944"))
 
 
 # Premium 4GB Renaming Client Config
-STRING_SESSION = os.environ.get("STRING_SESSION", "")
+STRING_SESSION = os.environ.get("STRING_SESSION", "BQGJgPEAFHyq1RsjTWWbIvXDYtn8FOR77WLHSnv_5B5rKiwURLEPp5vSP4e-6h-HjMftW9T8Q7DAMCEGD3ETRLuZKzw9Pncs74Zaq6LqkD7WImrfR25PgxEdfc-_pKQu6PnOejI8hX8wS-rTmGnjuQxR9nZ0yRX2pomd6Zi_EQwuTSEhxE6sXpOgxZfHX-zTOP5-pQGtQx3uvbr4Cct4_5haapQM1gZGVn6UBT4pCOpBNGVkJqNK1fydQHeZBU4QWr3dEcSrMEes6qNsZBZllA0BAAYJ6-cChHZoOYC7MudU1pxuBm2Xa2zwP3enhllBhCQUr3bp_xGQwP-00iLACDvdZ2s9CAAAAAGLIaJZAA")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "Aerobots_tm")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://TaktAsahina99:TaktAsahina99@cluster0.iq3cx2j.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "CodeXBots")
 
 
 # Other Variables Config
-START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ada3f739fed7efdbe7b08.jpg")
+START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/7a36b88ba7e2f54dbcab1.jpg")
